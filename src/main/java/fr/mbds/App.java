aa3049bd9957c6837acb8fb8b077ed206c7d7686
Daniel Arasu ANBARASU3 <anbarasu@polytech.unice.fr>
2019-05-30 13:00:32 +0530

    Detect optype of BulkItemResponse

--- src/main/java/fr/mbds/App.java
+++ src/main/java/fr/mbds/App.java
@@ -9,7 +9,15 @@ import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.DocWriteRequest.OpType;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.DocWriteResponse;
 
 import org.apache.http.HttpHost;
 
@@ -24,7 +32,7 @@ public class App {
 	private static PDFFilter filter = new PDFFilter();
 
 	private static RestHighLevelClient client = null;
-    private static  File once = null;
+    private static File once = null;
 	private static String onceText = null;
 
 	static {
@@ -34,19 +42,52 @@ public class App {
 		client = new RestHighLevelClient(clientBuilder);
 	}
 
-	public static void indexDoc(File file, String text) {
+	public static void indexDoc(BulkRequest br, File file, String text) {
 		IndexRequest req = new IndexRequest("cool","doc");
 		Map<String, Object> data = new HashMap<>();
 		data.put("filename",file.getName());
 		data.put("content", text);
 		req.source(data);
-		client.indexAsync(req, RequestOptions.DEFAULT, new IndexActionListener());		
+		br.add(req);
 
+	}
+
+	public static void printBulkResponseDetails(BulkResponse bulkResponse) {
+		BulkItemResponse[] bulkItemResponses = bulkResponse.getItems();
+		System.out.println(Arrays.toString(bulkItemResponses));
+		for(BulkItemResponse bir : bulkItemResponses) {
+			BulkItemResponse.Failure failure = bir.getFailure();
+			if(failure != null) {
+				System.err.println(failure.getMessage());
+			} else {
+				DocWriteResponse dwr = bir.getResponse();;
+				DocWriteRequest.OpType opType = bir.getOpType();
+				switch(opType) {
+					case CREATE:
+					case INDEX: {
+						IndexResponse indexResponse = (IndexResponse)dwr;
+						System.out.println(indexResponse);
+						break;
+					}
+					case UPDATE: {
+						UpdateResponse updateResponse = (UpdateResponse)dwr;
+						System.out.println(updateResponse);
+						break;
+					}
+					case DELETE: {
+						DeleteResponse deleteResponse = (DeleteResponse)dwr;
+						System.out.println(deleteResponse);
+						break;
+					}
+				}
+			}			
+		}
 	}
 
     public static void main( String[] args ) {
     	try {
 	    	Path startPath = FileSystems.getDefault().getPath("D:","attestations_stage");
+	    	final BulkRequest bulkRequest = new BulkRequest();
 	        Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
 
 	        	@Override
@@ -65,7 +106,7 @@ public class App {
 	        				System.out.println("=====");
 	        				System.out.println(text);
 	        				
-							indexDoc(file, text);
+							indexDoc(bulkRequest, file, text);
 
 	        			}
 	        			doc.close();
@@ -74,7 +115,9 @@ public class App {
 	        	}
 
 	        });
-	        // client.close();
+	        BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+	        printBulkResponseDetails(bulkResponse);
+	        client.close();
 	    } catch(IOException ioe) {
 	    	ioe.printStackTrace();
 	    }
